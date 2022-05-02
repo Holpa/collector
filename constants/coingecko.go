960908cb982062fb1bc6d454f9ee7e44e76da696
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2022-05-02 19:57:28 +0200

    HOPS-32: Parse Coingecko ID + currency from string

--- constants/coingecko.go
+++ constants/coingecko.go
@@ -1,5 +1,7 @@
 package constants
 
+import "strings"
+
 type (
 	CoinGeckoId       string
 	CoinGeckoCurrency string
@@ -18,3 +20,49 @@ const (
 	COINGECKO_USD CoinGeckoCurrency = "usd"
 	COINGECKO_EUR CoinGeckoCurrency = "eur"
 )
+
+func CoingeckoIdFromString(value string, fallback CoinGeckoId) CoinGeckoId {
+	lowerCased := strings.ToLower(value)
+
+	switch lowerCased {
+	case "avax":
+		return COINGECKO_AVAX
+	case "fly":
+		return COINGECKO_FLY
+	default:
+		return fallback
+	}
+}
+func (coingeckoId CoinGeckoId) String() string {
+	switch coingeckoId {
+	case COINGECKO_AVAX:
+		return "avax"
+	case COINGECKO_FLY:
+		return "fly"
+	default:
+		return "unknown-id"
+	}
+}
+
+func CoingeckoCurrenyFromString(value string) CoinGeckoCurrency {
+	lowerCased := strings.ToLower(value)
+
+	switch lowerCased {
+	case "usd":
+		return COINGECKO_USD
+	case "eur":
+		return COINGECKO_EUR
+	default:
+		return COINGECKO_USD
+	}
+}
+func (coingeckoCurrency CoinGeckoCurrency) String() string {
+	switch coingeckoCurrency {
+	case COINGECKO_USD:
+		return "usd"
+	case COINGECKO_EUR:
+		return "eur"
+	default:
+		return "unknown-currency"
+	}
+}
